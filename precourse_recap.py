1a2a3bfadb9a13fd3377dc6d9c5791c2c64429de
belus90 <belus90@gmail.com>
2022-02-21 16:56:14 +0000

    first comit

---
+++ precourse_recap.py
@@ -0,0 +1,23 @@
+number_1 = 12
+number_2 = 5
+number_3 = 2.5
+
+sum = number_1 + number_2
+sum_1 = number_1 * number_2
+sum_2 = number_1/number_2 
+
+word_1 = "Hello"
+word_2 = " My firs code"
+
+def sum_function(num_1 , num_2):      
+    return num_1 + num_2
+
+
+print(sum)
+print(sum_1)
+print(sum_2)
+
+print(word_1 + ":" +  word_2)
+print(word_2)
+
+print(sum_function(2,3))
